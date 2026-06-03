8474c73e88f32f716c82bfbd224ea4857dab9ea7
Kevin <kevin@Kevins-MacBook-Pro.local>
2026-06-03 17:05:08 +0900

    Introduce Quill.js rich text editor and update HTML sanitization

--- app/utils/sanitize.py
+++ app/utils/sanitize.py
@@ -1,16 +1,24 @@
 import bleach
 
-# Define allowed tags and attributes for sanitization
+# Define allowed tags and attributes for sanitization (Supports Quill.js)
 ALLOWED_TAGS = [
     "a", "abbr", "acronym", "b", "blockquote", "code", "em", "i", "li", "ol",
-    "strong", "ul", "p", "br", "span", "div", "h1", "h2", "h3", "h4", "h5", "h6"
+    "strong", "ul", "p", "br", "span", "div", "h1", "h2", "h3", "h4", "h5", "h6",
+    "img", "u", "s", "pre"
 ]
 
 ALLOWED_ATTRIBUTES = {
     "a": ["href", "title", "target"],
     "abbr": ["title"],
     "acronym": ["title"],
+    "img": ["src", "alt", "width", "height", "class"],
+    "span": ["class", "style"],
+    "div": ["class", "style"],
 }
+
+ALLOWED_STYLES = [
+    "color", "background-color", "text-align"
+]
 
 def sanitize_html(html_content: str) -> str:
     """
@@ -23,5 +31,6 @@ def sanitize_html(html_content: str) -> str:
         html_content,
         tags=ALLOWED_TAGS,
         attributes=ALLOWED_ATTRIBUTES,
+        styles=ALLOWED_STYLES,
         strip=True
     )
